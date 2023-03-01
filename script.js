e2681637ff50a722857a25034c9db2e20f171c52
EthanCombs <ethan.combs@stu.barren.kyschools.us>
2023-03-01 10:14:47 -0600

    finished

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ What would you like to do (Please enter one of the options below):
 
 // Array for storing tasks
 const tasks = [
-  `Charge MacBook`,
-  `Master JavaScript`
+    `Charge MacBook`,
+    `Master JavaScript`
 ];
 
 // For displaying tasks to the user
@@ -23,20 +23,43 @@ let showTasks = ``;
 let newTask;
 
 // For storing the number of the task to be removed
-let num;
+let num = 0;
+let c;
 
 // For storing the value of the removed task
 let removed;
 
 // Displays the menu for the user to select an option. Also, sets the user's response to the userInput variable.
-// ???
-let userInput = prompt(menu);
+let userInput = prompt(menu).toUpperCase();
 
-// Loops and continues to the menu until the user ends/closes the Task Manager (AKA Until the user enters CLOSE (???))
+// Loops and continues to the menu until the user ends/closes the Task Manager (AKA Until the user enters CLOSE)
 while (userInput !== `CLOSE`){
-
-  // This is can be commented out after the CLOSE feature is completed
-  break;
+    showTasks = ``;
+    for (index in tasks){
+        c = index;
+        c++;
+        showTasks += (`\n` + c + `: ` + tasks[index]);
+    }
+    if (userInput == `TASKS`){
+        alert(tasks.join(`\n`));
+    } else if (userInput == `NEW`){
+        newTask = ``;
+        while (newTask == ``){
+            newTask = prompt(`Please enter the new task: `);
+        }
+        tasks.push(newTask);
+        alert(newTask + ' has been added');
+    } else if (userInput == `REMOVE`){
+        num = prompt(`Please enter a number to remove: ` + showTasks);
+        while (num < 1 || num > c){
+            alert("Not a valid entry");
+            num = prompt(`Please enter a number to remove: ` + showTasks);
+        }
+        num--;
+        removed = tasks.splice(num, 1);
+        alert(`"` + removed + `" has been removed`);
+    }
+    userInput = prompt(menu).toUpperCase();
 }
 
 // Alerts the user that they have closed the program
